c79734dab965d5135d15382e3bd4b544531159d0
nene97 <nebojsavelkovski@live.com>
2024-03-03 19:47:47 +0100

    Homework C# class 02
    Homework C# class 02

---
+++ Homework C# class 02/Class 2 Homework/SwapNumbers/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Input the first number: ");
+int numOne = int.Parse(Console.ReadLine());
+Console.WriteLine("Input the second number: ");
+int numTwo = int.Parse(Console.ReadLine());
+Console.WriteLine("After swapping : ");
+Console.WriteLine((numOne, numTwo) = (numTwo, numOne));
