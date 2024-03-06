7c872a12a0df78fa2dedebee7ed0e6233f840bf7
nene97 <nebojsavelkovski@live.com>
2024-03-06 23:36:55 +0100

    Homework C# class 03
    Homework C# class 03

---
+++ Homework C# class 03/Homework C# class 03/SumOfEven/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+int[] arraysOfInt = new int[] {4 , 3 , 7 , 3 , 2 , 8};
+
+
+int sum = 0;
+int sumTwo = 0;
+
+
+foreach (int even in arraysOfInt)
+{
+    if (even % 2 == 0)
+    {
+        Console.WriteLine(even);
+        sum = sum + even;
+    }
+}
+Console.WriteLine(sum);
+
+
+
