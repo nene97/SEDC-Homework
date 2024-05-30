44295e9cc004ddffb7ea0fda014a3fd481240acb
nene97 <nebojsavelkovski@live.com>
2024-05-31 00:40:40 +0200

    HomeWork

---
+++ Homework DataBase/SQLQuery2.sql
@@ -0,0 +1,86 @@
+﻿
+
+
+
+--Requirements:
+--● Find all Students with FirstName = Antonio
+
+SELECT *
+FROM Student
+WHERE FirstName = 'Antonio'
+
+
+--● Find all Students with DateOfBirth greater than ‘01.01.1999’
+
+SELECT *
+FROM Student
+WHERE DateOfBirth > '01.01.1999'
+
+
+--● Find all Students with LastName starting With ‘J’ enrolled in January/1998
+
+
+SELECT *
+FROM Student
+WHERE LastName LIKE 'J%' AND EnrolledDate >= '01.01.1998' AND EnrolledDate < '01.02.1998'
+
+--● List all Students ordered by FirstName
+SELECT *
+FROM Student
+ORDER BY FirstName
+
+--● List all Teacher Last Names and Student Last Names in single result set.
+--Remove duplicates
+SELECT LastName FROM Teacher
+UNION
+SELECT LastName FROM Student
+
+
+--● Create Foreign key constraints from diagram or with script
+ALTER TABLE GradeDetails
+ADD CONSTRAINT FK_GradeDetails_Grade
+FOREIGN KEY (GradeID)
+REFERENCES Grade (ID)
+
+
+ALTER TABLE GradeDetails
+ADD CONSTRAINT FK_GradeDetails_AcheievmentType
+FOREIGN KEY (AchievementTypeID)
+REFERENCES AchievementType (ID)
+
+
+ALTER TABLE Grade
+ADD CONSTRAINT FK_Grade_Student
+FOREIGN KEY (StudentID)
+REFERENCES Student (ID)
+
+ALTER TABLE Grade
+ADD CONSTRAINT FK_Grade_Course
+FOREIGN KEY (CourseID)
+REFERENCES Course (ID)
+
+ALTER TABLE Grade
+ADD CONSTRAINT FK_Grade_Teacher
+FOREIGN KEY (TeacherID)
+REFERENCES Teacher (ID)
+
+--● List all possible combinations of Courses names and AchievementType names that can be passed by student
+
+SELECT C.[Name] AS CourseName , [AT].[Name] AS  AchievementType
+FROM Course C
+CROSS JOIN AchievementType [AT]
+ORDER BY C.[Name]
+
+
+--● List all Teachers without exam Grade
+
+
+SELECT T.ID, T.FirstName ,G.ID, G.TeacherID
+FROM Teacher T
+LEFT OUTER JOIN Grade G
+ON T.ID = G.TeacherID
+WHERE G.TeacherID IS NULL
+
+
+
+
---
+++ Homework DataBase/SQLQuery8.sql
@@ -0,0 +1,98 @@
+﻿--HOMEWORK REQUIREMENTS
+--● Calculate the count of all grades per Teacher in the system
+USE 
+SEDCACADEMYDB
+
+
+SELECT T.FirstName, COUNT(Grade) AS AllGradesPerTeacher
+FROM Grade G
+INNER JOIN Teacher T
+ON G.TeacherID = T.ID
+GROUP BY T.FirstName
+
+
+--● Calculate the count of all grades per Teacher in the system for first 100
+--Students (ID < 100)
+
+SELECT T.FirstName, COUNT(Grade) AS AllGradesPerTeacher
+FROM Grade G
+INNER JOIN Teacher T
+ON G.TeacherID = T.ID
+WHERE StudentID < 100
+GROUP BY T.FirstName
+
+
+--● Find the Maximal Grade, and the Average Grade per Student on all grades in
+--the system
+
+SELECT S.FirstName, MAX(Grade) AS MaximalGrade, AVG(Grade) AS AverageGrade
+FROM Grade G
+INNER JOIN Student S 
+ON G.StudentID = S.ID
+GROUP BY S.FirstName
+
+
+
+--● Calculate the count of all grades per Teacher in the system and filter only
+--grade count greater then 200
+
+SELECT T.FirstName, COUNT(Grade) AS AllGradesPerTeacher
+FROM Grade G
+INNER JOIN Teacher T
+ON G.TeacherID = T.ID
+GROUP BY T.FirstName
+HAVING COUNT(Grade) > 200
+
+
+--● Find the Grade Count, Maximal Grade, and the Average Grade per Student on
+--all grades in the system. Filter only records where Maximal Grade is equal to
+--Average Grade
+
+SELECT S.FirstName, MAX(Grade) AS MaximalGrade, AVG(Grade) AS AverageGrade ,COUNT(Grade) AS GradeCount
+FROM Grade G
+INNER JOIN Student S 
+ON G.StudentID = S.ID
+GROUP BY S.FirstName
+HAVING MAX(Grade) = AVG(Grade)
+--There are none
+
+
+--● List Student First Name and Last Name next to the other details from previous
+--query
+
+SELECT S.FirstName, S.LastName, MAX(Grade) AS MaximalGrade, AVG(Grade) AS AverageGrade ,COUNT(Grade) AS GradeCount
+FROM Grade G
+INNER JOIN Student S 
+ON G.StudentID = S.ID
+GROUP BY S.FirstName , S.LastName
+
+--● Create new view (vv_StudentGrades) that will List all StudentIds and count of
+--Grades per student
+
+CREATE VIEW vv_StudentGrades
+AS
+SELECT S.ID, COUNT(Grade) AS GradeCount
+FROM Grade G
+INNER JOIN Student S 
+ON G.StudentID = S.ID
+GROUP BY S.ID
+
+SELECT *
+FROM  vv_StudentGrades
+
+
+--● Change the view to show Student First and Last Names instead of StudentID
+ALTER VIEW vv_StudentGrades
+AS
+SELECT S.FirstName, S.LastName, COUNT(Grade) AS GradeCount
+FROM Grade G
+INNER JOIN Student S 
+ON G.StudentID = S.ID
+GROUP BY S.FirstName, S.LastName
+
+
+--● List all rows from view ordered by biggest Grade Count
+
+SELECT *
+FROM  vv_StudentGrades
+ORDER BY GradeCount DESC
