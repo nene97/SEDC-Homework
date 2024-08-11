a5af76465e78a4bf889bcee46a5276c4f0eb4880
nene97 <nebojsavelkovski@live.com>
2024-08-11 22:48:10 +0200

    HomeWorkClass02

---
+++ ASP.NET WebAPI/ASP.NET WebAPI Homework Class 02/HomeworkClass02/HomeworkClass02/Controllers/UsersController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace HomeworkClass02.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        //Add GET method that returns all users
+
+
+        [HttpGet("GetAllUsers")]
+        public ActionResult<string> GetAll()
+        {
+            return Ok(StaticDb.UserNames);
+        }
+
+        //Add GET method that returns one user
+        //by id
+        //it can be by a set number
+        //it can be random
+
+        [HttpGet("GetOneUserById")]
+        public ActionResult GetOneUser(int id)
+        {
+            try
+            {
+                if (id < 0)
+                {
+                    
+                    return BadRequest("The index has negative value");
+                }
+
+                if (id >= StaticDb.UserNames.Count)
+                {
+                    return StatusCode(StatusCodes.Status404NotFound, $"There is no user  on id  {id}");
+                }
+               
+                return StatusCode(StatusCodes.Status200OK, StaticDb.UserNames[id]);
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occured. Please contact your administrator");
+            }
+        }
+
+        [HttpGet("GetOneUserRandom")]
+        public ActionResult GetOneUserRandom()
+        {
+            try
+            {
+                Random rnd = new Random();
+
+                int random = rnd.Next(0, 5);
+
+               return Ok(StaticDb.UserNames[random]);
+
+             
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occured. Please contact your administrator");
+            }
+        }
+
+
+    }
+}
---
+++ ASP.NET WebAPI/ASP.NET WebAPI Homework Class 02/HomeworkClass02/HomeworkClass02/StaticDb.cs
@@ -0,0 +1,14 @@
+﻿namespace HomeworkClass02
+{
+    public static class StaticDb
+    {
+        public static List<string> UserNames = new List<string> 
+        { 
+            "UserName1",
+            "UserName2",
+            "UserName3",
+            "UserName4",
+            "UserName5"
+        };
+    }
+}
